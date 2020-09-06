aa670234b38edc62b63a4455f4b71e330214bf57
deepthi10-code <67965691+deepthi10-code@users.noreply.github.com>
2020-09-06 12:06:12 +0530

    Update deep_chatbot.py

--- deep_chatbot.py
+++ deep_chatbot.py
@@ -1,5 +1,5 @@
-'''pip install python-telegram-bot
-pip install adafruit-io '''
+!pip install python-telegram-bot
+!pip install adafruit-io
 
 import os
 YOUR_AIO_USERNAME = os.getenv('YOUR_AIO_USERNAME')  #ADAFRUIT_IO_USERNAME
@@ -21,13 +21,12 @@ def start(bot,update):
     update.message.reply_text("type /led_off to turn on the bulb")
     
 def led_off(bot,update):
-    value = Data(value=0) # Sending a value to a feed
+    value = Data(value=0) #Sending a value to a feed
     value_send = aio.create_data('ledbot1',value)
     chat_id = update.message.chat_id
     bot.send_message(chat_id=chat_id, text="light is turning off")
     bot.send_photo(chat_id,photo='https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png')
     bot.send_message(chat_id=chat_id, text="light turned off")
-
 
 def led_on(bot,update):
     value = Data(value=1)
