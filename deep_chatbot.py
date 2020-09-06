0ec38fd5142da62ae2bef0aee2df1889d928ab34
deepthi10-code <67965691+deepthi10-code@users.noreply.github.com>
2020-09-06 19:46:07 +0530

    Update deep_chatbot.py

--- deep_chatbot.py
+++ deep_chatbot.py
@@ -16,25 +16,25 @@ from telegram.ext import Updater, CommandHandler , MessageHandler
 import requests  # Getting the data from the cloud
 
 def start(bot,update):
-    update.message.reply_text("Hi, I'M LED CONTROL CHATBOT")
-    update.message.reply_text("type /led_on to turn on the bulb")
-    update.message.reply_text("type /led_off to turn on the bulb")
+    update.message.reply_text('Hi, IM LED CONTROL CHATBOT')
+    update.message.reply_text('type /led_on to turn on the bulb')
+    update.message.reply_text('type /led_off to turn on the bulb')
     
 def led_off(bot,update):
     value = Data(value=0) #Sending a value to a feed
     value_send = aio.create_data('ledbot',value)
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="light is turning off")
+    bot.send_message(chat_id=chat_id, text='light is turning off')
     bot.send_photo(chat_id,photo='https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png')
-    bot.send_message(chat_id=chat_id, text="light turned off")
+    bot.send_message(chat_id=chat_id, text='light turned off')
 
 def led_on(bot,update):
     value = Data(value=1)
     value_send = aio.create_data('ledbot',value)
     chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="light is turning on")
+    bot.send_message(chat_id=chat_id, text='light is turning on')
     bot.send_photo(chat_id,photo='https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png')
-    bot.send_message(chat_id=chat_id, text="light turned on")
+    bot.send_message(chat_id=chat_id, text='light turned on')
 
 BOT_TOKEN= os.getenv('BOT_TOKEN')
 u = Updater(BOT_TOKEN, use_context=True)
