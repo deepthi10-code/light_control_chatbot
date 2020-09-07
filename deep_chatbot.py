fb88c4c61f11e5c67d29488ea38462cbe3f6c334
deepthi10-code <67965691+deepthi10-code@users.noreply.github.com>
2020-09-07 10:36:18 +0530

    Update deep_chatbot.py

--- deep_chatbot.py
+++ deep_chatbot.py
@@ -29,20 +29,24 @@ def led_off(bot,update):
     value_send = aio.create_data('ledbot',value)
     chat_id = bot.message.chat_id
     bot.message.reply_text('light is turning off')
-    file_id='https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png'
+    #file_id='https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png'
     #bot.send_photo(chat_id=chat_id,photo=file_id)
-    send_photo(chat_id, file_id, caption=None, disable_notification=False, reply_to_message_id=None, reply_markup=None, timeout=20, parse_mode=None, **kwargs)
+    '''send_photo(chat_id, file_id, caption=None, disable_notification=False, reply_to_message_id=None, reply_markup=None, timeout=20, parse_mode=None, **kwargs)
+    bot.message.reply_text('light turned off')'''
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png", caption="light turned off")
     
-    bot.message.reply_text('light turned off')
+    
 
 def led_on(bot,update):
     value = Data(value=1)
     value_send = aio.create_data('ledbot',value)
     chat_id = bot.message.chat_id
     bot.message.reply_text('light is turning on')
-    file_id='https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png'
-    send_photo(chat_id, file_id, caption=None, disable_notification=False, reply_to_message_id=None, reply_markup=None, timeout=20, parse_mode=None, **kwargs)
-    bot.message.reply_text('light turned on')
+    #file_id='https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png'
+    #send_photo(chat_id, file_id, caption=None, disable_notification=False, reply_to_message_id=None, reply_markup=None, timeout=20, parse_mode=None, **kwargs)
+    #bot.message.reply_text('light turned on')
+    
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png", caption="light turned on")
     
 def echo(bot, update):
     """Echo the user message."""
