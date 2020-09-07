5f22cc0eefdceea9eb27145f60b36d0b49fd812a
deepthi10-code <67965691+deepthi10-code@users.noreply.github.com>
2020-09-07 11:44:28 +0530

    Update deep_chatbot.py

--- deep_chatbot.py
+++ deep_chatbot.py
@@ -20,7 +20,7 @@ from telegram.ext import Updater, CommandHandler,MessageHandler, Filters
 import requests #Getting the data from the cloud
 
 def start(bot,update):
-    bot.message.reply_text('HI, IM LED CONTROL CHATBOT')
+    bot.message.reply_text('HI, IM LIGHT CONTROL CHATBOT')
     bot.message.reply_text('type /led_on to turn on the bulb')
     bot.message.reply_text('type /led_off to turn on the bulb')
     
@@ -28,15 +28,15 @@ def led_off(bot,update):
     value = Data(value=0) #Sending a value to a feed
     value_send = aio.create_data('ledbot',value)
     chat_id = bot.message.chat_id
-    bot.message.reply_text('light is turning off')
-    update.bot.sendPhoto(chat_id=chat_id, photo="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png", caption="light turned off")
+    bot.message.reply_text('Light is turning off... wait for a moment')
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png", caption="Light turned off!")
     
 def led_on(bot,update):
     value = Data(value=1)
     value_send = aio.create_data('ledbot',value)
     chat_id = bot.message.chat_id
-    bot.message.reply_text('light is turning on')
-    update.bot.sendPhoto(chat_id=chat_id, photo="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940194wdc9uy3j5o.png", caption="light turned on")
+    bot.message.reply_text('Light is turning on... wait for a moment')
+    update.bot.sendPhoto(chat_id=chat_id, photo="https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940194wdc9uy3j5o.png", caption="Light turned on!")
     
 def echo(bot, update):
     #Echo the user message
